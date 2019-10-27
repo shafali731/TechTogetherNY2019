42f6bf7ed5c22e2b1175802a9acb68af75109c50
Shafali Gupta <shafaligupta31@gmail.com>
2019-10-27 03:53:02 -0400

    urg

--- static/end.js
+++ static/end.js
@@ -1,5 +1,38 @@
 // var lastname = localStorage.getItem("leadership");
-//
+var differ = localStorage.getItem("differ");
+var career = localStorage.getItem("car");
+
+console.log(differ);
+var data = differ;
+console.log(data);
+var width = 420,
+	barHeight = 20;
+
+var x = d3.scaleLinear()
+	.domain([0, d3.max(data)])
+	.range([0, width]);
+
+var chart = d3.select(".chart")
+	.attr("width", width)
+	.attr("height", barHeight * data.length);
+
+var bar = chart.selectAll("g")
+	.data(data)
+	.enter().append("g")
+	.attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+bar.append("rect")
+	.attr("width", x)
+	.attr("height", barHeight - 1);
+
+bar.append("text")
+	.attr("x", function(d) { return x(d) - 3; })
+	.attr("y", barHeight/2 )
+	.attr("dy", ".35em")
+	.text(function(d) { return d; });
+
+
+// console.log(differ);
 // // console.log(lastname);
 // var chart = d3.bullet()
 //     .width(width)
